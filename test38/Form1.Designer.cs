87ad8dc52db6175ebf24c8d554650871eeb8e54a
adamyu <adamyxt@outlook.com>
2024-11-14 10:45:44 +0800

    添加项目文件。

---
+++ test38/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace test38
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStripContainer1 = new System.Windows.Forms.ToolStripContainer();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.文件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.退出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.系统管理ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.修改密码ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.药品管理ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.销售管理ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.报表管理ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.药品清单ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.药品信息输入ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripContainer1.BottomToolStripPanel.SuspendLayout();
+            this.toolStripContainer1.LeftToolStripPanel.SuspendLayout();
+            this.toolStripContainer1.TopToolStripPanel.SuspendLayout();
+            this.toolStripContainer1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStripContainer1
+            // 
+            // 
+            // toolStripContainer1.BottomToolStripPanel
+            // 
+            this.toolStripContainer1.BottomToolStripPanel.Controls.Add(this.statusStrip1);
+            // 
+            // toolStripContainer1.ContentPanel
+            // 
+            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(783, 403);
+            this.toolStripContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            // 
+            // toolStripContainer1.LeftToolStripPanel
+            // 
+            this.toolStripContainer1.LeftToolStripPanel.Controls.Add(this.toolStrip1);
+            this.toolStripContainer1.Location = new System.Drawing.Point(0, 0);
+            this.toolStripContainer1.Name = "toolStripContainer1";
+            this.toolStripContainer1.Size = new System.Drawing.Size(800, 450);
+            this.toolStripContainer1.TabIndex = 0;
+            this.toolStripContainer1.Text = "toolStripContainer1";
+            // 
+            // toolStripContainer1.TopToolStripPanel
+            // 
+            this.toolStripContainer1.TopToolStripPanel.Controls.Add(this.menuStrip1);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripProgressBar1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 0);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 0;
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(51, 17);
+            this.toolStripStatusLabel1.Text = "页码1/1";
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.toolStripLabel2});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 3);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(17, 51);
+            this.toolStrip1.TabIndex = 0;
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(15, 17);
+            this.toolStripLabel1.Text = "A";
+            // 
+            // toolStripLabel2
+            // 
+            this.toolStripLabel2.Name = "toolStripLabel2";
+            this.toolStripLabel2.Size = new System.Drawing.Size(15, 17);
+            this.toolStripLabel2.Text = "B";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.文件ToolStripMenuItem,
+            this.系统管理ToolStripMenuItem,
+            this.药品管理ToolStripMenuItem,
+            this.销售管理ToolStripMenuItem,
+            this.报表管理ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 25);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 文件ToolStripMenuItem
+            // 
+            this.文件ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.退出ToolStripMenuItem});
+            this.文件ToolStripMenuItem.Name = "文件ToolStripMenuItem";
+            this.文件ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.文件ToolStripMenuItem.Text = "文件";
+            // 
+            // 退出ToolStripMenuItem
+            // 
+            this.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem";
+            this.退出ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.退出ToolStripMenuItem.Text = "退出";
+            // 
+            // 系统管理ToolStripMenuItem
+            // 
+            this.系统管理ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.修改密码ToolStripMenuItem});
+            this.系统管理ToolStripMenuItem.Name = "系统管理ToolStripMenuItem";
+            this.系统管理ToolStripMenuItem.Size = new System.Drawing.Size(68, 21);
+            this.系统管理ToolStripMenuItem.Text = "系统管理";
+            // 
+            // 修改密码ToolStripMenuItem
+            // 
+            this.修改密码ToolStripMenuItem.Name = "修改密码ToolStripMenuItem";
+            this.修改密码ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.修改密码ToolStripMenuItem.Text = "修改密码";
+            // 
+            // 药品管理ToolStripMenuItem
+            // 
+            this.药品管理ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.药品清单ToolStripMenuItem,
+            this.药品信息输入ToolStripMenuItem});
+            this.药品管理ToolStripMenuItem.Name = "药品管理ToolStripMenuItem";
+            this.药品管理ToolStripMenuItem.Size = new System.Drawing.Size(68, 21);
+            this.药品管理ToolStripMenuItem.Text = "药品管理";
+            // 
+            // 销售管理ToolStripMenuItem
+            // 
+            this.销售管理ToolStripMenuItem.Name = "销售管理ToolStripMenuItem";
+            this.销售管理ToolStripMenuItem.Size = new System.Drawing.Size(68, 21);
+            this.销售管理ToolStripMenuItem.Text = "销售管理";
+            // 
+            // 报表管理ToolStripMenuItem
+            // 
+            this.报表管理ToolStripMenuItem.Name = "报表管理ToolStripMenuItem";
+            this.报表管理ToolStripMenuItem.Size = new System.Drawing.Size(68, 21);
+            this.报表管理ToolStripMenuItem.Text = "报表管理";
+            // 
+            // 药品清单ToolStripMenuItem
+            // 
+            this.药品清单ToolStripMenuItem.Name = "药品清单ToolStripMenuItem";
+            this.药品清单ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.药品清单ToolStripMenuItem.Text = "药品清单";
+            // 
+            // 药品信息输入ToolStripMenuItem
+            // 
+            this.药品信息输入ToolStripMenuItem.Name = "药品信息输入ToolStripMenuItem";
+            this.药品信息输入ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.药品信息输入ToolStripMenuItem.Text = "药品信息输入";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.toolStripContainer1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "多窗口案例";
+            this.toolStripContainer1.BottomToolStripPanel.ResumeLayout(false);
+            this.toolStripContainer1.BottomToolStripPanel.PerformLayout();
+            this.toolStripContainer1.LeftToolStripPanel.ResumeLayout(false);
+            this.toolStripContainer1.LeftToolStripPanel.PerformLayout();
+            this.toolStripContainer1.TopToolStripPanel.ResumeLayout(false);
+            this.toolStripContainer1.TopToolStripPanel.PerformLayout();
+            this.toolStripContainer1.ResumeLayout(false);
+            this.toolStripContainer1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStripContainer toolStripContainer1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 文件ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel2;
+        private System.Windows.Forms.ToolStripMenuItem 退出ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 系统管理ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 药品管理ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 销售管理ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 报表管理ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 修改密码ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 药品清单ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 药品信息输入ToolStripMenuItem;
+    }
+}
+
---
+++ test38/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test38
+{
+    public partial class Form1 : Form
+    {
+        Form frm2, frm3, frm4;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // 为所有菜单项（包括子菜单项）绑定统一的点击事件
+            BindMenuItems(menuStrip1.Items);
+        }
+
+        // 递归绑定点击事件
+        private void BindMenuItems(ToolStripItemCollection menuItems)
+        {
+            foreach (ToolStripItem item in menuItems)
+            {
+                if (item is ToolStripMenuItem menuItem)
+                {
+                    // 为每个菜单项绑定点击事件
+                    menuItem.Click += MenuItem_Click;
+
+                    // 如果该菜单项有子菜单（下拉菜单），递归绑定子菜单项
+                    if (menuItem.DropDownItems.Count > 0)
+                    {
+                        BindMenuItems(menuItem.DropDownItems);
+                    }
+                }
+            }
+        }
+
+        // 统一的菜单项点击事件处理方法
+        private void MenuItem_Click(object sender, EventArgs e)
+        {
+            ToolStripMenuItem clickedItem = sender as ToolStripMenuItem;
+            if (clickedItem != null)
+            {
+                // 根据菜单项的名称或文本来判断是哪个菜单项被点击了
+                switch (clickedItem.Text)
+                {
+                    case "退出":
+                        MessageBox.Show("File menu clicked!");
+                        break;
+                    case "修改密码":
+                        if(frm2==null|| frm2.IsDisposed)
+                        {
+                            frm2 = new Form2();
+                            frm2.TopLevel = false;
+                            frm2.Parent = toolStripContainer1.ContentPanel;
+                            frm2.Show();
+                        }
+                        break;
+                    case "药品清单":
+                        if (frm3 == null || frm3.IsDisposed)
+                        {
+                            frm3 = new Form3();
+                            frm3.MdiParent = this;
+                            frm3.Parent = toolStripContainer1.ContentPanel;
+                            frm3.Show();
+                        }
+                        break;
+                    case "药品信息输入":
+                        MessageBox.Show("View menu clicked!");
+                        break;
+                }
+            }
+        }
+    }
+}
